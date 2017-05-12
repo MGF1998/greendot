5754bfcf297c265d3453b3e4d9c0938f217d2bdf
FelixRilling <contact@f-rilling.com>
2017-05-12 11:22:55 +0200

    added jquery to npm deps

--- ui/index.js
+++ ui/index.js
@@ -1,10 +1,18 @@
-const {ipcRenderer} = require("electron");
-const sendAsync = function(channel,arg){
-    ipcRenderer.send(channel,arg);
-    console.log("sending async message "+arg+" on channel "+channel);
+const $ = require("jquery");
+const {
+    ipcRenderer
+} = require("electron");
+const sendAsync = function (channel, arg) {
+    ipcRenderer.send(channel, arg);
+    console.log("sending async message " + arg + " on channel " + channel);
 };
 
-$("#closewin").click(()=>{sendAsync("system","close");});
-$("#minwin").click(()=>{sendAsync("system","min");});
-$("#maxwin").click(()=>{sendAsync("system","max");});
-
+$("#closewin").click(() => {
+    sendAsync("system", "close");
+});
+$("#minwin").click(() => {
+    sendAsync("system", "min");
+});
+$("#maxwin").click(() => {
+    sendAsync("system", "max");
+});
